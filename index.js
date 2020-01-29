13e683611905fdda4d9b4208c833f601c5c82e54
thsvr <vr.ths.zd@gmail.com>
2020-01-29 16:59:16 -0300

    Add form and form toggle button

--- index.js
+++ index.js
@@ -3,30 +3,34 @@ let booksSection = document.getElementById("books");
 const bookAdder = (book, index) => {
   return `<article class='book' id='book-${index}'>
   <h3>${book.title}</h3>
+  <p><small> ${book.read ? "Already read" : "Not read yet"}</small></p>
   <p><small>${book.author} - <em>${book.year}</em></small></p>
   <figure>
     <img class='book-image' src='${book.cover}' alt="${book.title}'s cover">
   </figure>
+  <p><small>Number of pages: ${book.pageNumber}</small></p>
   </article>`;
 };
 
 let myLibrary = [];
 
-function Book(title, author, year, cover) {
+function Book(title, author, year, cover, pageNumber, read = false) {
   this.title = title;
   this.author = author;
   this.year = year;
   this.cover = cover;
+  this.pageNumber = pageNumber;
+  this.read = read;
 };
 
 const addBookToLibrary = (book, library, Book) => {
-  const { title, author, year, cover } = book;
-  let newBook = new Book(title, author, year, cover);
+  const { title, author, year, cover, pageNumber, read } = book;
+  let newBook = new Book(title, author, year, cover, pageNumber, read);
   library.push(newBook);
 };
 
-addBookToLibrary({ title: "Killing Eve", author: 'Christina Chen', year: '2005', cover: 'https://http2.mlstatic.com/dvd-killing-eve-1-temporada-completa-frete-gratis-D_NQ_NP_926551-MLB28004445126_082018-F.jpg' }, myLibrary, Book);
-addBookToLibrary({ title: "Autonomous", author: 'Christina Chen', year: '2005', cover: 'https://http2.mlstatic.com/dvd-killing-eve-1-temporada-completa-frete-gratis-D_NQ_NP_926551-MLB28004445126_082018-F.jpg' }, myLibrary, Book);
+addBookToLibrary({ title: "Killing Eve", author: 'Christina Chen', year: '2005', cover: 'https://http2.mlstatic.com/dvd-killing-eve-1-temporada-completa-frete-gratis-D_NQ_NP_926551-MLB28004445126_082018-F.jpg', pageNumber: 15, read: true }, myLibrary, Book);
+addBookToLibrary({ title: "Autonomous", author: 'Christina Chen', year: '2005', cover: 'https://http2.mlstatic.com/dvd-killing-eve-1-temporada-completa-frete-gratis-D_NQ_NP_926551-MLB28004445126_082018-F.jpg', pageNumber: 15 }, myLibrary, Book);
 
 const render = library => {
   library.forEach((book, i) => {
@@ -35,3 +39,7 @@ const render = library => {
 };
 render(myLibrary);
 console.log(myLibrary)
+
+const toggleForm = () => {
+    document.getElementById("book-form").classList.toggle("hidden-form")
+}

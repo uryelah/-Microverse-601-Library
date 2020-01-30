4dd84c7f817cdef91a4125fd29aa3020434b5b9b
sarah <sarachamorro@gmail.com>
2020-01-30 12:50:09 -0300

    Fix index.js linting and add togle hide button for form

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-let booksSection = document.getElementById("books");
-let localLibrary = localStorage.getItem('myLibrary');
+const booksSection = document.getElementById('books');
+const localLibrary = localStorage.getItem('myLibrary');
 
+const getStoredLibrary = () => {
+  // get latested localStorage values
+  // parse string into array
+  JSON.parse(localStorage.getItem('myLibrary'));
+};
 
-const bookAdder = (book, index) => {
-  return `<article class='book' id='book-${index}'>
+const storeLibrary = (library) => {
+  // parse array into string and save it again in the locastorage
+  localStorage.setItem('myLibrary', JSON.stringify(library));
+};
+
+const bookAdder = (book, index) => `<article class='book' id='book-${index}'>
   <h3>${book.title}</h3>
-  <p><small> ${book.read ? "Already read" : "Not read yet"}</small></p>
+  <p><small> ${book.read ? 'Already read' : 'Not read yet'}</small></p>
   <p><small>${book.author} - <em>${book.year}</em></small></p>
   <figure>
     <img class='book-image' src='${book.cover}' alt="${book.title}'s cover">
@@ -13,10 +22,9 @@ const bookAdder = (book, index) => {
   <p><small>Number of pages: ${book.pageNumber}</small></p>
   <button type="button" onclick="deleteBook(${index})" >Delete</button>
   <button type="button" onclick="updateBook(${index}, 'read')" >${
-    book.read ? "Mark unread book" : "Mark book as read"
-  }</button>
+  book.read ? 'Mark unread book' : 'Mark book as read'
+}</button>
   </article>`;
-};
 
 function Book(title, author, year, cover, pageNumber, read = false) {
   this.title = title;
@@ -28,104 +36,77 @@ function Book(title, author, year, cover, pageNumber, read = false) {
 }
 
 const addBookToLibrary = (book, library, Book) => {
-  let parsedLibrary = library;
-  const { title, author, year, cover, pageNumber, read } = book;
-  let newBook = new Book(title, author, year, cover, pageNumber, read);
+  const parsedLibrary = library;
+  const {
+    title, author, year, cover, pageNumber, read,
+  } = book;
+  const newBook = new Book(title, author, year, cover, pageNumber, read);
   parsedLibrary.push(newBook);
-  localStorage.setItem('myLibrary', JSON.stringify(parsedLibrary))
+  storeLibrary(parsedLibrary);
 };
-/*
-addBookToLibrary(
-  {
-    title: "Killing Eve",
-    author: "Christina Chen",
-    year: "2005",
-    cover:
-      "https://http2.mlstatic.com/dvd-killing-eve-1-temporada-completa-frete-gratis-D_NQ_NP_926551-MLB28004445126_082018-F.jpg",
-    pageNumber: 15,
-    read: true
-  },
-  myLibrary,
-  Book
-);
-addBookToLibrary(
-  {
-    title: "Autonomous",
-    author: "Christina Chen",
-    year: "2005",
-    cover:
-      "https://http2.mlstatic.com/dvd-killing-eve-1-temporada-completa-frete-gratis-D_NQ_NP_926551-MLB28004445126_082018-F.jpg",
-    pageNumber: 15
-  },
-  myLibrary,
-  Book
-);
-*/
+
 const render = library => {
   // if library exists in locastorage iteratate through each book else create it
   if (library) {
-    let currentLibrary = JSON.parse(library)
+    const currentLibrary = JSON.parse(library);
     currentLibrary.forEach((book, i) => {
       booksSection.innerHTML = bookAdder(book, i) + booksSection.innerHTML;
     });
   } else {
-    localStorage.setItem('myLibrary', JSON.stringify([]));
+    storeLibrary([]);
   }
 };
 
 render(localLibrary);
 
 const toggleForm = () => {
-  document.getElementById("book-form").classList.toggle("hidden-form");
+  const bookForm = document.getElementById('book-form').classList;
+
+  if (bookForm.contains('hidden-form')) {
+    document.getElementById('toggle-form').classList.add('hidden-form');
+  } else {
+    document.getElementById('toggle-form').classList.remove('hidden-form');
+  }
+
+  bookForm.toggle('hidden-form');
 };
 
-document.getElementById("book-form").addEventListener("submit", event => {
-  localLibrary = localStorage.getItem('myLibrary');
-  let newBook = {};
+document.getElementById('book-form').addEventListener('submit', event => {
+  const library = getStoredLibrary();
+  const newBook = {};
 
   [...event.target].forEach(input => {
-    if (input.type === "submit") {
+    if (input.type === 'submit') {
       return;
-    } else {
-      newBook[input.name] = input.value;
     }
+    newBook[input.name] = input.value;
   });
 
   // Add new Book with values from form to myLibrary array
-  addBookToLibrary(newBook, JSON.parse(localLibrary), Book);
+  addBookToLibrary(newBook, library, Book);
   // Get updated localStorage values
-  localLibrary = localStorage.getItem('myLibrary');
-  updateLibrary = JSON.parse(localLibrary);
-  booksSection.innerHTML =
-    bookAdder(updateLibrary[updateLibrary.length - 1], updateLibrary.length - 1) +
-    booksSection.innerHTML;
+  const updateLibrary = getStoredLibrary();
+  booksSection.innerHTML = bookAdder(updateLibrary[updateLibrary.length - 1],
+    updateLibrary.length - 1) + booksSection.innerHTML;
 
   event.preventDefault();
 });
 
 const deleteBook = index => {
-  // get latested localStorage values
-  localLibrary = localStorage.getItem('myLibrary');
-  // parse string into array
-  let updateLibrary = JSON.parse(localLibrary);
+  const updateLibrary = getStoredLibrary();
   // take item out of array
   updateLibrary.splice(index, 1);
-  // parse array into string and save it again in the locastorage
-  localStorage.setItem('myLibrary', JSON.stringify(updateLibrary));
-  document.getElementById(`book-${index}`).outerHTML = "";
+  storeLibrary(updateLibrary);
+  document.getElementById(`book-${index}`).outerHTML = '';
 };
 
 const updateBook = (index, field) => {
-  // get latested localStorage values
-  localLibrary = localStorage.getItem('myLibrary');
-  // parse string into array
-  let updateLibrary = JSON.parse(localLibrary);
+  const updateLibrary = getStoredLibrary();
   // update value of read as oposite of current value
   updateLibrary[index][field] = !updateLibrary[index][field];
-  // parse array into string and save it again in the locastorage
-  localStorage.setItem('myLibrary', JSON.stringify(updateLibrary));
+  storeLibrary(updateLibrary);
   document.getElementById(`book-${index}`).outerHTML = bookAdder(
     updateLibrary[index],
-    index
+    index,
   );
 };
